572d97a5c171fe787d0895bd0f1c7d26799f204e
leofss <leofernandess11@gmail.com>
2022-07-20 09:46:34 -0300

    Setando rota teste

--- src/app.ts
+++ src/app.ts
@@ -3,12 +3,18 @@ import config from "config";
 
 const app = express();
 
+//Routes
+import router from "./router";
+
+//prefixo de URL
+app.use("/api/",router);
+
 //JSON midleware para poder trafegar informações com esse tipo de dado
-app.use(express.json())
+app.use(express.json());
 
 //app port 
-const port = config.get<number>("port")
+const port = config.get<number>("port");
 
 app.listen(port, async() =>{
-    console.log(`App na porta ${port} ok`)
+    console.log(`App na porta ${port} ok`);
 })
